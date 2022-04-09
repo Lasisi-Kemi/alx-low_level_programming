394b726153343d4259c7ff0b0ca6bca2d27bb79e
Lasisi-Kemi <opeyemioluwakemi6@gmail.com>
2022-04-09 11:43:29 -0700

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdioh.h>
+/**
+*
+* main - Entry point
+* Return: Always 0 (Success/correct)
+*/
+
+int main(void)
+
+{
+char alphebet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+if (alphabet == 'q')	
+continue;
+
+else if (alphabet == 'e')
+continue;
+
+putchar(alphabet);
+}
+
+putchar('\n')
+
+return (0);
+
+}
