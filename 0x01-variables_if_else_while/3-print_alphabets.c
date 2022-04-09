a75a39618a1fea8925c9290450dd1330c1da01c5
Lasisi-Kemi <opeyemioluwakemi6@gmail.com>
2022-04-09 05:22:54 -0700

    print alphas

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,21 +7,19 @@
 int main(void)
 
 {
-char upper_case;
 char lower_case;
-
-for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-{
-putchar(upper_case);
-}
+char upper_case;
 
 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
 putchar(lower_case);
 }
 
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+{
+putchar(upper_case);
+}
+
 putchar ('\n');
 
-return (0);
-
-}
+return (0)
