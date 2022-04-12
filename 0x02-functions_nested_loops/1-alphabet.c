d16de986cf19e7028edbb204b6f4f178bbc364a9
Lasisi-Kemi <opeyemioluwakemi6@gmail.com>
2022-04-12 15:16:31 -0700

    small letter

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*main - check the code
+*
+*Return: Always 0.
+*/
+void print_alphabet(void)
+{
+char smalletter;
+
+for (smalletter = 'a'; smalleter <= 'z'; smalletter)
+_putchar(smalleter);
+
+_putchar('\n');
+}
