4a04801357f958e2f307facf7002ace0773a7301
Lasisi-Kemi <opeyemioluwakemi6@gmail.com>
2022-04-12 15:40:51 -0700

    lower case

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include main.h
+/**
+* main - print_alphabet - prints ten times the alphabet
+*/
+void print_alphabet_x10(void)
+{
+int count = 0;
+char smalletter;
+
+while (count++ <= 9)
+{
+for (smalletter = 'a'; smalletter = 'z'; smalletter++)
+_putchar(smalletter);
+_putchar('\n')
+}
+}
