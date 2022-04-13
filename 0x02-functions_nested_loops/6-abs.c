e70155f3d0b174827f0241fca1070fefee017f12
Lasisi-Kemi <opeyemioluwakemi6@gmail.com>
2022-04-12 23:32:01 -0700

    absolute integar

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _abs - Computes the absolute value of an integar
+* @n: The integerto be computed
+*
+* Return: The absolute value of the integar
+*/
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
