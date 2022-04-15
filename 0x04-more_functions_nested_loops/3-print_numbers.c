bde12712c4280717381d7b2aaff3fdf2dda1aadd
Lasisi-Kemi <opeyemioluwakemi6@gmail.com>
2022-04-15 06:55:21 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_numbers - print numbers
- * Return: Always 0.
- */
+* print_numbers - print numbers
+* Return: Always 0.
+*/
 void print_numbers(void)
 {
 int i;
@@ -15,4 +15,5 @@ i++;
 }
 
 _putchar ('\n');
+return (0);
 }
