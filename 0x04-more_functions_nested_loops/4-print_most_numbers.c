cad72cc85ab7a1bc71ad8a8a77c9a0cf70ef08e2
Lasisi-Kemi <opeyemioluwakemi6@gmail.com>
2022-04-15 07:33:01 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,18 +6,20 @@
  */
 void print_most_numbers(void)
 {
-
 int i;
 
 i = 0;
-while (i < 10)
-{
-if (i != 2 && i != 4)
-{
-_putchar (i + '0');
-}
-i++;
-}
 
+for (i = 0; i < 10; i++)
+{
+
+if (i == 2 || i == 4)
+
+i++;
+
+_putchar(i + '0');
+
+}
 _putchar ('\n');
+
 }
