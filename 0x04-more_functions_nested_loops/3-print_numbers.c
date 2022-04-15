2472cbb6a17c956cbb2829103c2fcf297746683e
Lasisi-Kemi <opeyemioluwakemi6@gmail.com>
2022-04-15 08:26:28 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,15 @@
 * print_numbers - print numbers
 * Return: Always 0.
 */
+
 void print_numbers(void)
 {
 int i;
-
 i = 0;
 
 for (i = 0; i < 10; i++)
 
-_putchar (i + '0';)
+_putchar (i + '0');
 
 _putchar ('\n');
 }
