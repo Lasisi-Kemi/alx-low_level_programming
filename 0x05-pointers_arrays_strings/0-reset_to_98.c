b459d1e03ebf666a3b1ce87f7faaf05d064bb6a8
Lasisi-Kemi <opeyemioluwakemi6@gmail.com>
2022-04-21 00:46:39 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - pointer to an integar parameter, updates to 98.
+ *
+ * @n: int parameter
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
