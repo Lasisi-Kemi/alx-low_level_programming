fc2057980a3e615dc4363a8af95e4c7f4ccc64b3
Lasisi-Kemi <opeyemioluwakemi6@gmail.com>
2022-04-23 14:31:18 -0700

    two string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,13 @@
 char *_strcat(char *dest, char *src)
 {
 int x, y;
+x = 0;
 
-for (x = 0; dest[x] != '\0'; x++)
-;
-for (y = 0; src[y] != '\0'; y++)
-{
-dest[x] = src[y];
-x++;
-}
-dest[x] = '\0';
+while (dest[c])
+c++;
+
+for (y = 0; src[y] ; y++)
+
+dest[x++] = src[y];
 return (dest);
 }
