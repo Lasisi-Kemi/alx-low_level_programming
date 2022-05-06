ce200002a45ca146380078efb9edbcf38b950e9f
Lasisi-Kemi <opeyemioluwakemi6@gmail.com>
2022-05-06 14:05:47 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints the number of arguments passed into it
- * @argc: holds the number of arguments passed
- * @argv: array pointer that holds the arguments passed
- * Returb: Always 0 (Success)
- */
+* main - a program that prints the number of arguments 
+* 	 passed into it
+* @argc: holds the number of arguments passed
+* @argv: array pointer that holds the arguments passed
+* Return: Always 0 (Success)
+*/
 
 int main(int argc, char __attribute__((unused)) *argv[])
 
