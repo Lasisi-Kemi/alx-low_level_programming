53df7ec40c437852182e98c3c13c913a1c1f9351
Lasisi-Kemi <opeyemioluwakemi6@gmail.com>
2022-05-13 15:03:52 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
 /**
- * print_name - function to print name
- * @name: name to printf
- * @f: function pointer that doesn't return anything
- * Return: Always 0 (Success)
- */
+* print_name - function to print name
+* @name: name to printf
+* @f: function pointer that doesn't return anything
+* Return: Always 0 (Success)
+*/
 
 void print_name(char *name, void (*f)(char *))
 
