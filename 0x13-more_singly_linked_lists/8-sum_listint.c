6be14e08f2d3eb1316e53cbe76f14595058b5c23
Lasisi-Kemi <91321941+Lasisi-Kemi@users.noreply.github.com>
2022-05-27 22:06:47 +0100

    Update 8-sum_listint.c
    sum list int

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -10,16 +10,14 @@
 */
 int sum_listint(listint_t *head)
 {
-	int sum = 0;
-
-	while (head != NULL)
-	{
-		/* add n to sum*/
-		sum += head->n;
-		/*go to the next node*/
-		head = head->next;
-	}
-
-	/*if list will be empty it will return sum = 0*/
-	return (sum);
+int sum = 0;
+while (head != NULL)
+{
+/* add n to sum*/
+sum += head->n;
+/*go to the next node*/
+head = head->next;
+}
+/*if list will be empty it will return sum = 0*/
+return (sum);
 }
